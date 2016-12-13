8b140bcfaeb74395fc2f9bdf8d496e41534545d6
phillip-tan <tan.phillipb@gmail.com>
2016-12-12 17:24:57 -0800

    datetime delta final

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,16 +1,30 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
-# 937 days 
+from datetime import date
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
-# 513 days
+####a)
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
-# 7850 days
+date_start = date(2013, 1, 2)
+date_stop = date(2015, 7, 28)
+delta_a = date_stop - date_start
+print delta_a.days
+
+####b)
+date_start = '12312013'
+date_stop = '05282015'
+
+date_start = date(2013, 12, 31)
+date_stop = date(2015, 5, 28)
+delta_b = date_stop - date_start
+print delta_b.days
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+
+date_start = date(1994, 1, 15)
+date_stop = date(2015, 7, 14)
+delta_c = date_stop - date_start
+print delta_c.days
