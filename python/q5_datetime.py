cdb423a69b9ff730d61ab4c3ff2b556b539a20c4
phillip-tan <tan.phillipb@gmail.com>
2016-12-12 17:22:03 -0800

    datetime delta

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -3,11 +3,14 @@
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+# 937 days 
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
+# 513 days
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+# 7850 days
