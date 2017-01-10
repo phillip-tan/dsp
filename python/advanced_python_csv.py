74fba6f23bc9620227e90b8088aa01df0adf15d5
Phillip Tan <tan.phillipb@gmail.com>
2017-01-09 22:42:07 -0800

    Added to advanced_python_csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,10 @@
-PLACE YOUR CODE HERE
+import re
+answer = []
+with open('./faculty.csv', 'r') as file_object:
+        lines_read = file_object.readlines()
+        for lines in lines_read:
+            m = re.search('.*,.*,.*,(.*)', lines)
+            email = m.group(1)
+            answer.append(email)
+        answer.remove(' email')
+        print answer
