35b5f2cd5090b7c928d1a490f78a8b8011e8c590
Phillip Tan <tan.phillipb@gmail.com>
2017-01-10 20:59:50 -0800

    added emails.csv and answered advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,10 +1,13 @@
 import re
 answer = []
 with open('./faculty.csv', 'r') as file_object:
-        lines_read = file_object.readlines()
-        for lines in lines_read:
-            m = re.search('.*,.*,.*,(.*)', lines)
-            email = m.group(1)
-            answer.append(email)
-        answer.remove(' email')
-        print answer
+    lines_read = file_object.readlines()
+    for lines in lines_read:
+        m = re.search('.*,.*,.*,(.*)', lines)
+        email = m.group(1)
+        answer.append(email)
+    answer.remove(' email')
+
+    solution = open('emails.csv', 'w')
+    for each in answer:
+        solution.write(each + '\n')
